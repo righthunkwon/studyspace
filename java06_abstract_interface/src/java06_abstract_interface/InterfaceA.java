b60ddded5a242f93f00b9863b29af9db0add934a
kwonjeonghun <righthun.kwon@gmail.com>
2023-01-11 14:07:58 +0900

    2023.1.11

--- java06_abstract_interface/src/java06_abstract_interface/InterfaceA.java
+++ java06_abstract_interface/src/java06_abstract_interface/InterfaceA.java
@@ -20,8 +20,7 @@ package java06_abstract_interface;
 			인터페이스 선언
 				: 자바에서 인터페이스를 선언하는 방법은 클래스를 작성하는 방법과 같다.
 				  인터페이스를 선언할 때에는 접근 제어자와 함께 interface 키워드를 사용하면 된다.
-				: 단, 클래스와는 달리 
-				  인터페이스의 모든 필드는 public static final이어야 하며, 
+				: 단, 클래스와는 달리 인터페이스의 모든 필드는 public static final이어야 하며, 
 				  모든 메소드는 public abstract이어야 한다.
 
 						접근제어자 interface 인터페이스이름 {
@@ -32,21 +31,16 @@ package java06_abstract_interface;
 						}
 
 
-				  
-
 			인터페이스 구현
-				: 인터페이스는 추상 클래스와 마찬가지로 자신이 직접 인스턴스를 생성할 수는 없습니다.
-				  따라서 인터페이스가 포함하고 있는 추상 메소드를 구현해 줄 클래스를 작성해야만 합니다.
+				: 인터페이스는 추상 클래스와 마찬가지로 자신이 직접 인스턴스를 생성할 수는 없다.
+				  따라서 인터페이스가 포함하고 있는 추상 메소드를 구현해 줄 클래스를 작성해야 한다.
+				  또한, 만약 모든 추상 메소드를 구현하지 않는다면, abstract 키워드를 사용하여 추상 클래스로 선언해야 한다.
 				
-				자바에서 인터페이스는 다음과 같은 문법을 통해 구현합니다.
-				
-				class 클래스이름 implements 인터페이스이름 { ... }
-				
-				만약 모든 추상 메소드를 구현하지 않는다면, abstract 키워드를 사용하여 추상 클래스로 선언해야 합니다.
+						class 클래스이름 implements 인터페이스이름 { ... }
 	*/
 
 
-public interface InterfaceA {
+public interface InterfaceA extends InterfaceB {
 	// 상수
 	public static final int MAX = 100;
 	public static final String FIRST_NAME = "LEE";
