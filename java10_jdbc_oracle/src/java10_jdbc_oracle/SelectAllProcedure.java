e59bbcb9ea10d8b1d18d5aaf228956c56bc8d80b
hippoDev <righthun.kwon@gmail.com>
2023-02-10 17:49:22 +0900

    2023.2.10 java - sql(oracle)

--- java10_jdbc_oracle/src/java10_jdbc_oracle/SelectAllProcedure.java
+++ java10_jdbc_oracle/src/java10_jdbc_oracle/SelectAllProcedure.java
@@ -1,5 +1,9 @@
 package java10_jdbc_oracle;
 
+import java.sql.ResultSet;
+
+import oracle.jdbc.internal.OracleTypes;
+
 public class SelectAllProcedure extends DBConnection {
 
 	public SelectAllProcedure() {
@@ -14,8 +18,20 @@ public class SelectAllProcedure extends DBConnection {
 			
 			cstmt = conn.prepareCall(sql);
 			
+			cstmt.registerOutParameter(1, OracleTypes.CURSOR);
 			
+			// 실행
+			cstmt.executeLargeUpdate();
 			
+			// 첫번째 ?에 담긴 레코드 꺼내기
+			rs = (ResultSet)cstmt.getObject(1);
+			
+			// b.isbn, b.bookname, b.price, b.pub_date, b.category, b.stock, a.author, p.pub_name, p.pub_tel
+			while (rs.next()) {
+				System.out.printf("%d, %s, %d, %s, %s, %d, %s, %s, %s\n", 
+								rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5), 
+								rs.getInt(6), rs.getString(7), rs.getString(8), rs.getString(9));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
