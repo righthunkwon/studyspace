5bc2929a501c5d350279658215c769128cacc820
hippoDev <righthun.kwon@gmail.com>
2023-04-20 11:30:56 +0900

    2023.4.20 js

--- D-Day-counter/index.js
+++ D-Day-counter/index.js
@@ -1,3 +1,8 @@
+const msgContainer = document.querySelector('#d-day-message');
+const container = document.querySelector('#d-day-container');
+// container.style.display = 'none';
+msgContainer.innerHTML = 'D-Day를 입력해 주세요.';
+
 const dateFormMaker = function () {
     const inputYear = document.querySelector('#target-year-input').value;
     const inputMonth = document.querySelector('#target-month-input').value;
@@ -15,15 +20,34 @@ const counterMaker = function () {
     const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0); // setHours(0, 0, 0, 0) : 기준 시간은 09시인데 이를 0시로 초기화
     const remaining = (targetDate - nowDate) / 1000;
     if (remaining <= 0) {
-        console.log("타이머가 종료되었습니다.");
+
     } else if (isNaN(remaining)) {
-        console.log("유효한 시간대가 아닙니다.");
+        msgContainer.innerHTML = '유효한 시간대가 아닙니다.';
     }
 
-    const remainingDate = Math.floor(remaining / 3600 / 24);
-    const remainingHours = Math.floor(remaining / 3600) % 24;
-    const remainingMin = Math.floor(remaining / 60) % 60;
-    const remainingSec = Math.floor(remaining) % 60;
+    const remainingObj = {
+        remainingDate: Math.floor(remaining / 3600 / 24),
+        remainingHours: Math.floor(remaining / 3600) % 24,
+        remainingMin: Math.floor(remaining / 60) % 60,
+        remainingSec: Math.floor(remaining) % 60
+    }
 
-    console.log(remainingDate, remainingHours, remainingMin, remainingSec);
+    const documentObj = {
+        days: document.getElementById('days'),
+        hours: document.getElementById('hours'),
+        min: document.getElementById('min'),
+        sec: document.getElementById('sec')
+    }
+
+    const timeKeys = Object.keys(remainingObj);
+
+    for (let i = 0; i < 10; i = i + 1) {
+
+    }
+
+    documentObj.days.textContent = remainingObj.remainingDate;
+    documentObj.hours.textContent = remainingObj.remainingHours;
+    documentObj.min.textContent = remainingObj.remainingMin;
+    documentObj.sec.textContent = remainingObj.remainingSec;
+
 }
