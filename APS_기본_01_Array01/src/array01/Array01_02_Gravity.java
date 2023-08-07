8bd74d57cf0d76e2c003716b164f1eb254ca8197
조아영 <aystarr@naver.com>
2023-08-07 17:01:03 +0900

    array, bubble sort

---
+++ APS_기본_01_Array01/src/array01/Array01_02_Gravity.java
@@ -0,0 +1,9 @@
+package array01;
+
+public class Array01_02_Gravity {
+	public static void main(String[] args) {
+		int[] box = {7,4,2,0,0,6,0,7,0};
+		// 2차원으로 보이지만 실제로는 1차원으로 봐도 무방
+		// 회전할 필요가 없다.
+	}
+}
