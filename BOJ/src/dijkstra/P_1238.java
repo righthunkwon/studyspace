306eb3038e990c6d8c6e5e52dfb593932cd90ac6
righthunkwon <righthun.kwon@gmail.com>
2023-10-05 15:10:06 +0900

    boj solve

--- BOJ/src/dijkstra/P_1238.java
+++ BOJ/src/dijkstra/P_1238.java
@@ -1,10 +1,8 @@
 package dijkstra;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
