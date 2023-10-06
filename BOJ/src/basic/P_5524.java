44ce4e4eeb865f3ecfc970d3c587d973ef62287d
righthunkwon <righthun.kwon@gmail.com>
2023-10-06 15:37:04 +0900

    boj solve

---
+++ BOJ/src/basic/P_5524.java
@@ -0,0 +1,15 @@
+package basic;
+
+import java.util.Scanner;
+
+// 입실 관리
+public class P_5524 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			String s = sc.next();
+			System.out.println(s.toLowerCase());
+		}
+	}
+}
