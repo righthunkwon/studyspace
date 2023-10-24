2dd3728b9ae875e3068f48504aa2ffdc6a0dd474
righthunkwon <righthun.kwon@gmail.com>
2023-10-24 09:47:54 +0900

    boj solve

--- BOJ/src/basic/P_11942.java
+++
@@ -1,8 +0,0 @@
-package basic;
-
-// 고려대는 사랑입니다
-public class P_11942 {
-	public static void main(String[] args) {
-		System.out.println("고려대학교");
-	}
-}
--- BOJ/src/basic/P_14645.java
+++
@@ -1,9 +0,0 @@
-package basic;
-
-// 와이버스 부릉부릉
-// 입력을 아예 안 받고 정답만 출력해도 맞았다.
-public class P_14645 {
-	public static void main(String[] args) {
-		System.out.println("비와이");
-	}
-}
