f456c53e3be281256f69d06174581c98a0e20b38
righthunkwon <righthun.kwon@gmail.com>
2023-11-03 09:12:31 +0900

    db

--- SSAFIT/ssafit_data.sql
+++ SSAFIT/ssafit_data.sql
@@ -1,11 +1,28 @@
 -- (1) 사용자 데이터 삽입
 INSERT INTO `user` 
 VALUES 
-("ssafyid1", "pwd1", "김싸피", 1, "토끼", '1995-05-13'),
-("ssafyid2", "pwd2", "박싸피", 1, "두루미", '2000-12-17'),
-("ssafyid3", "pwd3", "서싸피", 2, "호랑이", '1998-01-12');
+("ssafyid1", "pwd1", "김싸피", "토끼", '1995-05-13'),
+("ssafyid2", "pwd2", "박싸피", "두루미", '2000-12-17'),
+("ssafyid3", "pwd3", "서싸피", "호랑이", '1998-01-12');
 
--- (2) 리뷰 데이터 삽입
+-- (2) 비디오 데이터 삽입
+INSERT INTO `video`
+VALUES (1, '가슴 운동 5가지 - 강경원 루틴', '벤치프레스와 덤벨프레스를 기반으로 한 가슴운동', '가슴', '강경원', 'https://www.youtube.com/watch?v=iv8reqGucYE&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', 0, SYSDATE());
+
+INSERT INTO `video` (`title`, `content`, `part`, `creator`, `link`, `regdate`)
+VALUES 
+('벤치프레스 완벽 가이드 - 강경원 방식', '벤치프레스를 기반으로 한 가슴운동', '가슴', '강경원', 'https://www.youtube.com/watch?v=A2kHURY746E&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90',  SYSDATE()),
+('인클라인 벤치프레스, 윗가슴을 채우는 가장 좋은 운동', '벤치프레스를 기반으로 한 가슴운동', '가슴', '강경원', 'https://www.youtube.com/watch?v=4HvI_mFhzVQ&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', SYSDATE()),
+('오랜만에 상의 탈의하고 가슴 운동', '벤치프레스를 기반으로 한 가슴운동', '가슴', '강경원', 'https://www.youtube.com/watch?v=-ZHWut0mS-c&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', SYSDATE());
+
+INSERT INTO `video` (`title`, `content`, `part`, `creator`, `link`, `regdate`)
+VALUES 
+('어깨운동제목', '어깨운동내용', '어깨', '강경원', '링크1',  SYSDATE()),
+('하체운동제목1', '하체운동내용', '하체', '강경원', '링크2', SYSDATE()),
+('하체운동제목2', '하체운동내용', '하체', '강경원', '링크3', SYSDATE());
+
+
+-- (3) 리뷰 데이터 삽입
 -- 각각 부모키가 user / video에 있음
 INSERT INTO `review`  
 VALUES 
@@ -20,22 +37,14 @@ VALUES
 SELECT * FROM review;
 DELETE FROM review;
 
--- (3) 비디오 데이터 삽입
-INSERT INTO `video`
-VALUES (1, '가슴 운동 5가지 - 강경원 루틴', '벤치프레스와 덤벨프레스를 기반으로 한 가슴운동', '가슴', 'https://www.youtube.com/watch?v=iv8reqGucYE&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', SYSDATE());
-
-INSERT INTO `video` (`title`, `content`, `part` , `link`, `regdate`)
-VALUES 
-('벤치프레스 완벽 가이드 - 강경원 방식', '벤치프레스를 기반으로 한 가슴운동', '가슴', 'https://www.youtube.com/watch?v=A2kHURY746E&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', SYSDATE()),
-('인클라인 벤치프레스, 윗가슴을 채우는 가장 좋은 운동', '벤치프레스를 기반으로 한 가슴운동', '가슴', 'https://www.youtube.com/watch?v=4HvI_mFhzVQ&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', SYSDATE()),
-('오랜만에 상의 탈의하고 가슴 운동', '벤치프레스를 기반으로 한 가슴운동', '가슴', 'https://www.youtube.com/watch?v=-ZHWut0mS-c&ab_channel=%EA%B0%95%EA%B2%BD%EC%9B%90', SYSDATE());
-
 -- 데이터 삽입 확인
 SELECT * FROM user;
 SELECT * FROM review;
 SELECT * FROM video;
+SELECT * FROM wishlist;
 
 -- 데이터 삭제(초기화)
 DELETE FROM user;
 DELETE FROM review;
-DELETE FROM movie;
+DELETE FROM video;
+DELETE FROM wishlist;
--- SSAFIT/ssafit_schema.sql
+++ SSAFIT/ssafit_schema.sql
@@ -10,9 +10,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 `user_id` varchar(20) NOT NULL,  -- 사용자 아이디(PK)
 `password` varchar(20) NOT NULL, -- 사용자 비밀번호
 `name` varchar(20) NOT NULL, -- 사용자 이름
-`gender` INT, -- 사용자 성별. 남자:1 , 여자:2
 `nickname` VARCHAR(20) NOT NULL, -- 사용자 닉네임
-`birthday` DATE, -- 사용자 생일
+`birthdate` DATE, -- 사용자 생일
 PRIMARY KEY(`user_id`));
 
 -- 영상 테이블 생성
@@ -20,8 +19,10 @@ CREATE TABLE IF NOT EXISTS `video` (
 `video_id` INT AUTO_INCREMENT NOT NULL, -- 영상 고유번호(PK)
 `title` varchar(30) NOT NULL, -- 영상 제목
 `content` TEXT NOT NULL, -- 영상 내용
-`part` VARCHAR(10) NOT NULL, -- 운동 부위
+`part` VARCHAR(20) NOT NULL, -- 운동 부위
+`creator` VARCHAR(20) NOT NULL, -- 운동 영상 크리에이터
 `link` VARCHAR(300) NOT NULL, -- URL
+`viewcnt` INT DEFAULT 0,
 `regdate` DATETIME DEFAULT NOW(), -- 영상 등록일
  PRIMARY KEY(`video_id`)
  );
@@ -29,18 +30,30 @@ CREATE TABLE IF NOT EXISTS `video` (
 -- 리뷰 테이블 생성
 CREATE TABLE IF NOT EXISTS `review` ( 
 `review_id` INT AUTO_INCREMENT NOT NULL, -- 리뷰 고유번호(PK) 
-`user_id` varchar(50) NOT NULL, -- 사용자 아이디(FK)
+`user_id` varchar(20) NOT NULL, -- 사용자 아이디(FK)
 `video_id` INT NOT NULL, -- 영상 고유번호(FK)
 `title` varchar(30) NOT NULL, -- 리뷰 제목
 `content` TEXT NOT NULL, -- 리뷰 내용
-`viewcnt` INT NOT NULL, -- 리뷰 조회수
+`viewcnt` INT DEFAULT 0, -- 리뷰 조회수
 `regdate` DATETIME DEFAULT NOW(), -- 리뷰 둥록일(기본값은 현재 시간)
 PRIMARY KEY(`review_id`),
-CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`)  -- 사용자의 키와 댓글 속성중에서 외래키를 연결
+CONSTRAINT `fk_user_id_review` FOREIGN KEY (`user_id`)  -- 사용자의 키와 댓글 속성중에서 외래키를 연결
         REFERENCES `user` (`user_id`),
-CONSTRAINT `fk_video_id` FOREIGN KEY (`video_id`)  -- 영상의 키와 댓글 속성중에서 외래키를 연결
+CONSTRAINT `fk_video_id_review` FOREIGN KEY (`video_id`)  -- 영상의 키와 댓글 속성중에서 외래키를 연결
         REFERENCES `video` (`video_id`)
 );
+
+CREATE TABLE IF NOT EXISTS `wishlist` (
+`wishlist_id` INT AUTO_INCREMENT NOT NULL,
+`user_id` varchar(20) NOT NULL, -- 사용자 아이디(FK)
+`video_id` INT NOT NULL, -- 영상 고유번호(FK)
+PRIMARY KEY(`wishlist_id`),
+CONSTRAINT `fk_user_id_wishlist` FOREIGN KEY (`user_id`)  -- 사용자의 키와 댓글 속성중에서 외래키를 연결
+    REFERENCES `user` (`user_id`),
+CONSTRAINT `fk_video_id_wishlist` FOREIGN KEY (`video_id`)  -- 영상의 키와 댓글 속성중에서 외래키를 연결
+	REFERENCES `video` (`video_id`)
+);
+
 
 -- 아래는 Debug용으로 사용한 SQL문
 -- 데이버 베이스 목록 조회
@@ -50,3 +63,4 @@ SHOW DATABASES;
 DESC user; -- 사용자 테이블 확인
 DESC video; -- 영상 테이블 확인
 DESC review; -- 리뷰 테이블 확인
+DESC wishlist; -- 위시리스트 테이블 확인
